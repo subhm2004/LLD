451b3bd0ed1106611b1eaa0605714cd7a46858f5
Shubham Malik <subhu04012003@gmail.com>
2026-07-01 01:11:32 +0530

    Refactor HttpRequest and HttpRequestStepBuilder for improved readability and consistency; add virtual destructors to step interfaces

--- L28 Builder_design_pattern/C++ Code/StepBuilder.cpp
+++ L28 Builder_design_pattern/C++ Code/StepBuilder.cpp
@@ -17,41 +17,47 @@
 
 using namespace std;
 
-class HttpRequest {
+class HttpRequest
+{
 private:
     string url;
     string method;
     map<string, string> headers;
-    map<string,string> queryParams;
+    map<string, string> queryParams;
     string body;
     int timeout; // in seconds
 
     // Private constructor - can only be accessed by the Builder
-    HttpRequest() { }
+    HttpRequest() {}
 
 public:
     friend class HttpRequestStepBuilder;
 
     // Method to execute the HTTP request
-    void execute() {
+    void execute()
+    {
         cout << "Executing " << method << " request to " << url << endl;
-        
-        if (!queryParams.empty()) {
+
+        if (!queryParams.empty())
+        {
             cout << "Query Parameters:" << endl;
-            for (const auto& param : queryParams) {
+            for (const auto &param : queryParams)
+            {
                 cout << "  " << param.first << "=" << param.second << endl;
             }
         }
 
         cout << "Headers:" << endl;
-        for (const auto& header : headers) {
+        for (const auto &header : headers)
+        {
             cout << "  " << header.first << ": " << header.second << endl;
         }
-        
-        if (!body.empty()) {
+
+        if (!body.empty())
+        {
             cout << "Body: " << body << endl;
         }
-        
+
         cout << "Timeout: " << timeout << " seconds" << endl;
         cout << "Request executed successfully!" << endl;
     }
@@ -63,90 +69,107 @@ class HeaderStep;
 class OptionalStep;
 
 // Step interfaces (Abstract classes)
-class UrlStep {
+class UrlStep
+{
 public:
-    virtual MethodStep& withUrl(const string& url) = 0;
+    // ese bhi hum virtual destructor define karte hai taki derived class ka destructor call ho sake jab base class pointer se delete kiya jaye.
+    virtual ~UrlStep() = default;
+    // virtual ~UrlStep() {}
+    virtual MethodStep &withUrl(const string &url) = 0;
 };
 
-class MethodStep {
+class MethodStep
+{
 public:
-    virtual HeaderStep& withMethod(string method) = 0;
+    virtual ~MethodStep() {}
+    virtual HeaderStep &withMethod(string method) = 0;
 };
 
-class HeaderStep {
+class HeaderStep
+{
 public:
-    virtual OptionalStep& withHeader(string key, string value) = 0;
+    virtual ~HeaderStep() {}
+    virtual OptionalStep &withHeader(string key, string value) = 0;
 };
 
-class OptionalStep {
+class OptionalStep
+{
 public:
     virtual ~OptionalStep() {}
-    virtual OptionalStep& withBody(const string& body) = 0;
-    virtual OptionalStep& withTimeout(int timeout) = 0;
+    virtual OptionalStep &withBody(const string &body) = 0;
+    virtual OptionalStep &withTimeout(int timeout) = 0;
     virtual HttpRequest build() = 0;
 };
 
 // Concrete step builder that implements all steps
-class HttpRequestStepBuilder : 
-    public UrlStep, 
-    public MethodStep, 
-    public HeaderStep,
-    public OptionalStep {
+class HttpRequestStepBuilder : public UrlStep,
+                               public MethodStep,
+                               public HeaderStep,
+                               public OptionalStep
+{
 
 private:
     HttpRequest req;
-    
-public:    
+
+public:
     // UrlStep implementation
-    MethodStep& withUrl(const string& url) override {
+    MethodStep &withUrl(const string &url) override
+    {
         req.url = url;
         return *this;
     }
-    
+
     // MethodStep implementation
-    HeaderStep& withMethod(string method) override {
+    HeaderStep &withMethod(string method) override
+    {
         req.method = method;
         return *this;
     }
-    
+
     // OptionalStep implementation
-    OptionalStep& withHeader(string key, string value) override {
+    OptionalStep &withHeader(string key, string value) override
+    {
         req.headers[key] = value;
         return *this;
     }
-    
-    OptionalStep& withBody(const string& body) override {
+
+    OptionalStep &withBody(const string &body) override
+    {
         req.body = body;
         return *this;
     }
-    
-    OptionalStep& withTimeout(int timeout) override {
+
+    OptionalStep &withTimeout(int timeout) override
+    {
         req.timeout = timeout;
         return *this;
     }
-    
-    HttpRequest build() override {
-        if (req.url.empty()) {
+
+    HttpRequest build() override
+    {
+        if (req.url.empty())
+        {
             throw runtime_error("URL cannot be empty");
         }
         return req;
     }
-    
+
     // Static method to start the building process
-    static UrlStep& getBuilder() {
+    static UrlStep &getBuilder()
+    {
         return *(new HttpRequestStepBuilder());
     }
 };
 
-int main() {
+int main()
+{
     HttpRequest stepRequest = HttpRequestStepBuilder::getBuilder()
-    .withUrl("https://api.example.com/products")
-    .withMethod("POST")
-    .withHeader("Content-Type", "application/json")
-    .withBody("{\"product\": \"Laptop\", \"price\": 49999}")
-    .withTimeout(45)
-    .build();
+                                  .withUrl("https://api.example.com/products")
+                                  .withMethod("POST")
+                                  .withHeader("Content-Type", "application/json")
+                                  .withBody("{\"product\": \"Laptop\", \"price\": 49999}")
+                                  .withTimeout(45)
+                                  .build();
 
     stepRequest.execute();
 }
-    

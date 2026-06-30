cb17203b6e4ec04c3e018b4f92896fd808c578e2
Shubham Malik <subhu04012003@gmail.com>
2026-06-30 23:02:59 +0530

    Refactor HttpRequest and HttpRequestBuilder classes for improved readability and consistency

--- L28 Builder_design_pattern/C++ Code/BuilderPattern.cpp
+++ L28 Builder_design_pattern/C++ Code/BuilderPattern.cpp
@@ -14,112 +14,127 @@
 
 using namespace std;
 
-class HttpRequest {
+class HttpRequest
+{
 private:
     string url;
     string method;
     map<string, string> headers;
-    map<string,string> queryParams;
+    map<string, string> queryParams;
     string body;
     int timeout; // in seconds
 
     // Private constructor - can only be accessed by the Builder (isse kya hoga ki new keyboard se HttpRequest object nahi banega jse ki hum ne singleton me bhi kiya tha ctor ko private kr diya tha, sirf builder se hi banega)
-    HttpRequest() { }
+    HttpRequest() {}
 
 public:
     // Declare the Builder class as a friend (isse kya hoga ki builder class ke methods ko access milega private members ka)
     friend class HttpRequestBuilder;
 
     // Method to execute the HTTP request
-    void execute() {
+    void execute()
+    {
         cout << "Executing " << method << " request to " << url << endl;
-        
-        if (!queryParams.empty()) {
+
+        if (!queryParams.empty())
+        {
             cout << "Query Parameters:" << endl;
-            for (const auto& param : queryParams) {
+            for (const auto &param : queryParams)
+            {
                 cout << "  " << param.first << "=" << param.second << endl;
             }
         }
 
         cout << "Headers:" << endl;
-        for (const auto& header : headers) {
+        for (const auto &header : headers)
+        {
             cout << "  " << header.first << ": " << header.second << endl;
         }
-        
-        if (!body.empty()) {
+
+        if (!body.empty())
+        {
             cout << "Body: " << body << endl;
         }
-        
+
         cout << "Timeout: " << timeout << " seconds" << endl;
         cout << "Request executed successfully!" << endl;
     }
 };
 
-class HttpRequestBuilder {
-private:    
+class HttpRequestBuilder
+{
+private:
     HttpRequest req;
 
-public:    
-    // Method chaining bolte hai isko 
+public:
+    // Method chaining bolte hai isko
     // with laga kr naam dena ek namining convention hai builder pattern me, jisse pata chale ki ye method object ko modify kar rahi hai aur builder object return kar rahi hai
     // withUrl, withMethod, withHeader, withQueryParams, withBody, withTimeout methods return karte hai reference to the builder object itself (isse kya hoga ki hum ek hi line me multiple methods call kar sakte hai aur ek fluent API ban jata hai)
-    HttpRequestBuilder& withUrl(const string& u) {
-        req.url = u; 
+    HttpRequestBuilder &withUrl(const string &u)
+    {
+        req.url = u;
         return *this;
     }
 
-    HttpRequestBuilder& withMethod(string method) {
+    HttpRequestBuilder &withMethod(string method)
+    {
         req.method = method;
         return *this;
     }
-    
-    HttpRequestBuilder& withHeader(const string& key, const string& value) {
+
+    HttpRequestBuilder &withHeader(const string &key, const string &value)
+    {
         req.headers[key] = value;
         return *this;
     }
 
-    HttpRequestBuilder& withQueryParams(const string& key, const string& value) {
+    HttpRequestBuilder &withQueryParams(const string &key, const string &value)
+    {
         req.queryParams[key] = value;
         return *this;
     }
-    
-    HttpRequestBuilder& withBody(const string& body) {
+
+    HttpRequestBuilder &withBody(const string &body)
+    {
         req.body = body;
         return *this;
     }
-    
-    HttpRequestBuilder& withTimeout(int timeout) {
+
+    HttpRequestBuilder &withTimeout(int timeout)
+    {
         req.timeout = timeout;
         return *this;
     }
-    
+
     // Build method to create the immutable HttpRequest object
-    HttpRequest build() {
+    HttpRequest build()
+    {
         // Validation logic can be added here
-        if (req.url.empty()) {
+        if (req.url.empty())
+        {
             throw runtime_error("URL cannot be empty");
         }
-        if (req.method.empty()) {
+        if (req.method.empty())
+        {
             throw runtime_error("HTTP method cannot be empty");
-        }   
-        
+        }
+
         return req;
     }
 };
 
-
-int main() {
+int main()
+{
     // Using Builder Pattern (nested class)
     HttpRequest request = HttpRequestBuilder()
-        .withUrl("https://api.example.com")
-        .withMethod("POST")
-        .withHeader("Content-Type", "application/json")
-        .withHeader("Accept", "application/json")
-        .withQueryParams("key", "12345")
-        .withBody("{\"name\": \"Aditya\"}")
-        .withTimeout(60)
-        .build();
+                              .withUrl("https://api.example.com")
+                              .withMethod("POST")
+                              .withHeader("Content-Type", "application/json")
+                              .withHeader("Accept", "application/json")
+                              .withQueryParams("key", "12345")
+                              .withBody("{\"name\": \"Shubham\"}")
+                              .withTimeout(60)
+                              .build();
 
     request.execute(); // Guaranteed to be in a consistent state
 }
-
